15093c0a29c80179b5f008630bd42585af86d868
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-10-12 02:25:30 +0700

    develop: sidebar component and update page

--- frontend/src/app/admin/order/page.tsx
+++ frontend/src/app/admin/order/page.tsx
@@ -2,7 +2,7 @@ import Order from '~/components/order'
 import { orders } from '~/data/orders'
 import { cn } from '~/lib/utils'
 
-export default function OrderPage() {
+export default function Page() {
   return (
     <main className=''>
       <section className={'grid grid-cols-3'}>
