419e0e1584c0b876526f79d2aab2911a65c924d9
Tung Pho <ductungpho1005@gmail.com>
2024-10-14 22:48:27 +0700

    bugfix

--- backend/src/models/schemas/bookings.schema.ts
+++ backend/src/models/schemas/bookings.schema.ts
@@ -1,7 +1,6 @@
 import { ObjectId } from 'mongodb'
 
 interface BookingType {
-  booking_id: ObjectId
   customer_name: string
   customer_phone: string
   table_number: number
@@ -10,7 +9,6 @@ interface BookingType {
 }
 
 export default class Booking {
-  booking_id: ObjectId
   customer_name: string
   customer_phone: string
   table_number: number
@@ -18,7 +16,6 @@ export default class Booking {
   details?: string
 
   constructor(booking: BookingType) {
-    this.booking_id = booking.booking_id
     this.customer_name = booking.customer_name
     this.customer_phone = booking.customer_phone
     this.table_number = booking.table_number
