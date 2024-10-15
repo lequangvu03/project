6ef116c835f2c10668bc0bbf408c739c4f48ab83
Tung Pho <ductungpho1005@gmail.com>
2024-10-15 18:11:10 +0700

    fix schema inventory and inventory logs

--- backend/src/models/schemas/inventoryLogs.schema.ts
+++ backend/src/models/schemas/inventoryLogs.schema.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from 'mongodb'
 
 interface InventoryLogType {
+  _id?: ObjectId
   item_id: ObjectId
   change_type: string
   change_quantity: number
@@ -8,12 +9,14 @@ interface InventoryLogType {
 }
 
 export default class InventoryLog {
+  _id?: ObjectId
   item_id: ObjectId
   change_type: string
   change_quantity: number
   change_date: Date
 
   constructor(log: InventoryLogType) {
+    this._id = log._id
     this.item_id = log.item_id
     this.change_type = log.change_type
     this.change_quantity = log.change_quantity
