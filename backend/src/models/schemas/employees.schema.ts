4481e4fffadf96a3f703da1268f3ebb60c3bda5c
Tung Pho <ductungpho1005@gmail.com>
2024-10-15 14:31:47 +0700

    update employees validators

--- backend/src/models/schemas/employees.schema.ts
+++ backend/src/models/schemas/employees.schema.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from 'mongodb'
 
 interface EmployeeType {
+  _id?: ObjectId
   name: string
   position: string
   salary: number
@@ -8,12 +9,14 @@ interface EmployeeType {
 }
 
 export default class Employee {
+  _id?: ObjectId
   name: string
   position: string
   salary: number
   contact_info: string
 
   constructor(employee: EmployeeType) {
+    this._id = employee._id
     this.name = employee.name
     this.position = employee.position
     this.salary = employee.salary
