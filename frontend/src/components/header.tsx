b27939e69358fb4110279fcce7d493b8f0965e07
Le Quang Vu <leevudev@gmail.com>
2024-11-09 13:41:03 +0700

    fix: conflic

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
 export default function Header() {
   return (
-    <header className='sticky top-6 z-10 flex h-[100px] items-center justify-between rounded-2xl bg-[var(--secondary-color)] px-4 shadow-sm shadow-white/20'>
+    <header className='sticky top-8 z-10 flex h-[100px] items-center justify-between rounded-2xl bg-[var(--secondary-color)] px-4 shadow-sm shadow-white/20'>
       <section className='flex flex-col'>
         <h2 className='text-[26px] font-semibold leading-8 text-gray-200'>Reservation</h2>
         <h4 className='text-[13px] font-medium leading-7 text-gray-500'>Friday, 1 Nov 2024</h4>
@@ -12,7 +12,7 @@ export default function Header() {
       <section className='flex items-center gap-[2px]'>
         <Link
           href='/admin/profile'
-          className='flex items-center justify-between gap-1 rounded-full bg-slate-600 px-4 py-2'
+          className='flex items-center justify-between gap-1 rounded-3xl bg-slate-600 px-2 py-1 transition-all hover:opacity-80'
         >
           <Avatar className='h-10 w-10'>
             <AvatarImage className='h-full w-full' src='https://github.com/shadcn.png' alt='@shadcn' />
