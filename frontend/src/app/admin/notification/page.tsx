37d6291b65406093edefdf313a175e55c9674e79
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-11-10 18:15:04 +0700

    develop

--- frontend/src/app/admin/notification/page.tsx
+++ frontend/src/app/admin/notification/page.tsx
@@ -7,12 +7,12 @@ export default function Page() {
     <main className='bg-[var(--secondary-color)]'>
       <section className='flex items-center justify-between rounded-sm p-4'>
         <div className='flex items-center gap-2'>
-          <Button className=''>ADD</Button>
-          <Button>Read</Button>
-          <Button>Unread</Button>
+          <Button className='bg-[#EA7C69]'>All</Button>
+          <Button className='bg-transparent text-gray-400'>Read</Button>
+          <Button className='bg-transparent text-gray-400'>Unread</Button>
         </div>
         <div>
-          <Button>Mark all as read</Button>
+          <Button className='bg-[#EA7C69]'>Mark all as read</Button>
         </div>
       </section>
       <section>
