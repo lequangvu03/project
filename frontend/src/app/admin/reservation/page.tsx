86d160f1f7f5b759c8e5079288acfc04e17aa5e8
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-11-11 17:43:08 +0700

    develop

--- frontend/src/app/admin/reservation/page.tsx
+++ frontend/src/app/admin/reservation/page.tsx
@@ -1,29 +1,68 @@
 'use client'
 
-import 'moment-timezone' // or 'moment-timezone/builds/moment-timezone-with-data[-datarange].js'. See their docs
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import React, { useCallback, useMemo, useState } from 'react'
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
 import moment from 'moment'
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment) // Khởi tạo localizer với moment
+const DragAndDropCalendar = withDragAndDrop(Calendar)
 
-// Set the IANA time zone you want to use
-moment.tz.setDefault('Europe/Paris')
-function ReservationPage() {
+interface Event {
+  id: number
+  title: string
+  start: Date
+  end: Date
+  allDay?: boolean
+}
 
-  const events = [
+const DragAndDrop: React.FC = () => {
+  const [myEvents, setMyEvents] = useState<Event[]>([
     {
-      title: 'My Event',
-      start: '2015-04-12T13:45:00-05:00',
-      end: '2015-04-12T14:00:00-05:00'
+      id: 0,
+      title: 'Sự kiện 1',
+      start: new Date(2023, 10, 1, 10, 0),
+      end: new Date(2023, 10, 1, 12, 0)
+    },
+    {
+      id: 1,
+      title: 'Sự kiện 2',
+      start: new Date(2023, 10, 2, 14, 0),
+      end: new Date(2023, 10, 2, 15, 0)
     }
-  ]
+  ])
 
+  const moveEvent = useCallback(
+    ({ event, start, end, isAllDay = false }: { event: Event; start: Date; end: Date; isAllDay?: boolean }) => {
+      const updatedEvent = { ...event, start, end, allDay: isAllDay }
+      setMyEvents((prevEvents) => prevEvents.map((ev) => (ev.id === event.id ? updatedEvent : ev)))
+    },
+    []
+  )
+
+  const resizeEvent = useCallback(({ event, start, end }: { event: Event; start: Date; end: Date }) => {
+    const updatedEvent = { ...event, start, end }
+    setMyEvents((prevEvents) => prevEvents.map((ev) => (ev.id === event.id ? updatedEvent : ev)))
+  }, [])
+
+  const defaultDate = useMemo(() => new Date(), [])
 
   return (
-    <div className='myCustomHeight'>
-      <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor='end' />
+    <div style={{ height: 600 }}>
+      <DragAndDropCalendar
+        defaultDate={defaultDate}
+        defaultView={Views.WEEK}
+        events={myEvents}
+        localizer={localizer} // Truyền localizer vào Calendar
+        // onEventDrop={moveEvent}
+        // onEventResize={resizeEvent}
+        resizable
+        popup
+      />
     </div>
   )
 }
 
-export default ReservationPage
+export default DragAndDrop
