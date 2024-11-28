afc66cd7e8ea1d739a1e2f9214b4cab0370664fa
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-11-28 21:27:36 +0700

    develop

--- frontend/src/components/dashboard-chart.tsx
+++ frontend/src/components/dashboard-chart.tsx
@@ -29,7 +29,7 @@ const chartConfig = {
 
 export default function DashboardChart() {
   return (
-    <div className='h-[200px] rounded-2xl bg-[var(--secondary-color)] p-4'>
+    <div className='rounded-2xl bg-[var(--secondary-color)] p-4'>
       <CardHeader>
         <CardTitle>Overview</CardTitle>
         <CardDescription>January - June 2024</CardDescription>
---
+++ frontend/src/components/report-item.tsx
@@ -0,0 +1,34 @@
+export default function ReportItem() {
+  return (
+    <div className='flex items-center justify-between'>
+      <div>
+        <h2>Reservation ID</h2>
+        <p>#12354564</p>
+      </div>
+      <div>
+        <h2>Reservation ID</h2>
+        <p>#12354564</p>
+      </div>
+      <div>
+        <h2>Reservation ID</h2>
+        <p>#12354564</p>
+      </div>
+      <div>
+        <h2>Reservation ID</h2>
+        <p>#12354564</p>
+      </div>
+      <div>
+        <h2>Reservation ID</h2>
+        <p>#12354564</p>
+      </div>
+      <div>
+        <h2>Reservation ID</h2>
+        <p>#12354564</p>
+      </div>
+      <div>
+        <h2>Reservation ID</h2>
+        <p>#12354564</p>
+      </div>
+    </div>
+  )
+}
