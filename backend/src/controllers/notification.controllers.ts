bed8122f613aeba9e51c0ca78922da7aa3410175
Tungle <tung291203@gmail.com>
2024-11-29 23:15:39 +0700

    pagination notification

--- backend/src/controllers/notification.controllers.ts
+++ backend/src/controllers/notification.controllers.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express'
 import notificationService from '~/services/notification.services'
 
 export const getAllNotificationController = async (req: Request, res: Response, error: NextFunction) => {
-  const result = await notificationService.getNotifications()
+  const limit = Number(req.query.limit)
+  const page = Number(req.query.page)
+  const result = await notificationService.getNotifications({ limit, page })
   return res.status(200).json({ message: 'get notification success', result })
 }
