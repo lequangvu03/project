40e408cea47c9d43038bafbc23553bee8e13c5c7
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-11-30 23:16:25 +0700

    develop

--- frontend/src/services/tables.services.ts
+++ frontend/src/services/tables.services.ts
@@ -1,26 +1,21 @@
-import { sendDelete, sendGet, sendPost, sendPut } from "~/api/request";
-
+import { sendDelete, sendGet, sendPost, sendPut } from '~/api/request'
 
 const tablesServices = {
-    getTables: () => {
-        return sendGet("/tables")
-    },
-    addTable: (body: {
-        table_number: number,
-        capacity: number,
-        location: string
-    }) => {
-        return sendPost("/table", body)
-    },
-    updateTable: ({ id, body }:
-        { id: string, body: { table_number: number,
-        capacity: number,
-        location: string} })=> {
-        sendPut(`/table/${id}`, body)
-    },
-    deleteTable: (id: string) => {
-        return sendDelete("/table"+ id)
-    }
+  getTables: () => {
+    return sendGet('/tables')
+  },
+  getTableById: (id: string) => {
+    return sendGet('/table/' + id)
+  },
+  addTable: (body: { table_number: number; capacity: number; location: string }) => {
+    return sendPost('/table', body)
+  },
+  updateTable: ({ id, body }: { id: string; body: { table_number: number; capacity: number; location: string } }) => {
+    return sendPut(`/table/${id}`, body)
+  },
+  deleteTable: (id: string) => {
+    return sendDelete('/table' + id)
+  }
 }
 
-export default tablesServices;
+export default tablesServices
