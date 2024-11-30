0d731351ea4987a105da2eba23559547d68e41f9
Tungle <tung291203@gmail.com>
2024-11-30 23:09:39 +0700

    noti

--- frontend/src/hooks/data/notifications.data.ts
+++ frontend/src/hooks/data/notifications.data.ts
@@ -1,12 +1,41 @@
-import { useQuery } from "@tanstack/react-query";
-import notificationsService from "~/services/notifications.services";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import notificationsService from '~/services/notifications.services'
 
-
-export const useGetNotificationAllQuery = function ({page,limit, status }: {page: number, limit: number, status: number}) {
-    return useQuery({
-        queryKey: ["NOTIFICATIONS", page, status],
-        queryFn: function () {
-            return notificationsService.getNotifications({ limit:limit, page: page, status: status })
-        }
-    })
+export const useGetNotificationAllQuery = function ({
+  page,
+  limit,
+  status
+}: {
+  page: number
+  limit: number
+  status: number
+}) {
+  return useQuery({
+    queryKey: ['NOTIFICATIONS', page, status],
+    queryFn: function () {
+      return notificationsService.getNotifications({ limit: limit, page: page, status: status })
+    }
+  })
+}
+export const useReadNotificationMutation = function () {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: notificationsService.readNotification,
+    onSuccess: function () {
+      queryClient.invalidateQueries({
+        queryKey: ['NOTIFICATIONS']
+      })
+    }
+  })
+}
+export const useMarkAllAsReadMutation = function () {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: notificationsService.markAllAsRead,
+    onSuccess: function () {
+      queryClient.invalidateQueries({
+        queryKey: ['READALLNOTIFICATIONS']
+      })
+    }
+  })
 }
